d337f6dffee111bb0616cd3cb4627f56da1fdad5
Kenny Katzgrau <ext.kkatzgrau@hexiscyber.com>
2015-06-06 09:45:25 -0400

    New build

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 BUILD=`php index.php build | tail -1`
-cp -r $BUILD /tmp
+
+rm -r /tmp/jerry-build
+mkdir /tmp/jerry-build
+cp -r $BUILD/* /tmp/jerry-build
 
 git checkout gh-pages
-cp -r /tmp/$BUILD/* .
+cp -r /tmp/jerry-build/* .
 git add .
 git commit -m "New build"
 git push origin gh-pages
